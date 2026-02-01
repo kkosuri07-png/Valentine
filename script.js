eb7785f728c355da62b2a234a4489044c079d652
kkosuri07-png <kkosuri.07@gmail.com>
2026-02-01 01:44:55 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,23 @@ function handleYesClick() {
   window.location.href = "yes_page.html";
 }
 
-function handleNoClick() {
-  alert("Nice try 😏");
-}
+const noBtn = document.querySelector(".no-button");
+const heading = document.querySelector("h1");
+
+const emojis = ["😏", "😂", "😜", "🙈", "🤭", "💃"];
+let emojiIndex = 0;
+
+const nopeSound = new Audio("engineer_no01");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 300 - 150;
+  const y = Math.random() * 300 - 150;
+
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+
+  heading.textContent = `Nice try ${emojis[emojiIndex]} 💘`;
+  emojiIndex = (emojiIndex + 1) % emojis.length;
+
+  nopeSound.currentTime = 0;
+  nopeSound.play();
+});
